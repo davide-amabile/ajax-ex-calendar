53121bb4d587365cfaf2e776d29bcb513af1c66c
davide-amabile <davideamabile81@gmail.com>
2020-09-18 16:48:51 +0200

    ciclo for

---
+++ js/script.js
@@ -0,0 +1,27 @@
+$(document).ready(function(){
+
+  // data iniziale
+  var dataIniziale =  moment("2018-1-1");
+
+  // variabile quanti giorni in un mese
+  var numGiorni = dataIniziale.daysInMonth();
+
+  // fare ciclo for per generare la lista dei giorni
+  for ( i=1; i < numGiorni; i++) {
+  // handlebars template
+  var source = $("calendario-template").html();
+  var template = Handlebars.compile(source);
+
+  var context = {
+    "day" : numGiorni[i],
+    "title": "january",
+    "body": dataIniziale.year()
+  };
+    var html = template(context);
+
+    $(".wrapper_main").append(html);
+  }
+  // fine ciclo
+
+});
+// fine documento jquery
